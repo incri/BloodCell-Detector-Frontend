f0ce6dc29cec1acf914b7df27757c90e24b9796d
incri <paudelbaibhav0@gmail.com>
2024-07-17 11:03:58 +0545

    complete refactor for registration hook to use react query

--- src/pages/registration/hooks/useResendEmail.ts
+++ src/pages/registration/hooks/useResendEmail.ts
@@ -5,15 +5,32 @@ interface UserData {
 }
 
 export const useResendEmail = () => {
-  const { loading, error, fetchData, response } = usePostData();
+  const mutation = usePostData();
 
   const resendEmail = async (userData: UserData) => {
-    return await fetchData<void>(
-      "/auth/users/resend_activation/",
-      "POST",
-      userData
-    );
+    try {
+      const response = await mutation.mutateAsync({
+        url: "/auth/users/resend_activation/",
+        method: "POST",
+        data: userData,
+      });
+
+      return response?.data; // Assuming response structure returns data directly
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error('Error resending email:', error.message);
+        throw error;
+      } else {
+        console.error('Unknown error occurred:', error);
+        throw new Error('An unknown error occurred.');
+      }
+    }
   };
 
-  return { loading, error, resendEmail, response };
+  return { 
+    loading: mutation.isPending, // Accessing loading state from useMutation
+    error: mutation.error as Error | undefined, // Accessing error state from useMutation
+    resendEmail, 
+    response: mutation.data // Accessing response data from useMutation
+  };
 };
